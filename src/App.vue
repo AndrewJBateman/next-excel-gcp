73d01af2129d6af93f6d0a9bcf742e8e87df7c35
Andrew Bateman <gomezbateman@yahoo.com>
2018-11-04 20:00:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-center" id="app">
+    <h1>Word Translator</h1>
+		<h5>Powered by 'Vue.js'</h5>
+		<hr>
+		<TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+		<TranslateOutput v-text="translatedText"></TranslateOutput>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
+
+export default {
+  name: 'app',
+  components:{
+	TranslateForm,
+	TranslateOutput
+	},
+	data: function(){
+		return {
+			translatedText: ''
+		}
+	},
+	methods: {
+		translateText: function(text, language){
+			this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181104T102320Z.a3fb9954e7544c8c.9b440f44d18ba210605f46825cd17fb833eaf247&lang='+language+'&text='+text)
+			.then((response) => {
+				this.translatedText = response.body.text[0];
+			});
+		}
+	}
+}
+</script>
+
+<style>
+body {
+  background: rgb(243, 233, 234);
+}
+</style>
